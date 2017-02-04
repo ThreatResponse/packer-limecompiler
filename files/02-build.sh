5a03c004a415ea8d9b33012d3847fc395180a4d5
Joel Ferrier <joel@ferrier.io>
2017-02-04 11:57:50 -0800

    use lime-compiler provided by installed gem

--- files/02-build.sh
+++ files/02-build.sh
@@ -10,12 +10,12 @@ gem install lime-compiler-*.gem
 
 if [ `echo $GPG_SIGN` == 1 ]; then
     echo "Spinning up with GPG Signing"
-    /opt/limecompiler/bin/lime-compiler -c /opt/limecompiler/conf/config.yml -m /opt/limecompiler/build/ -a /opt/limecompiler/archive/ --gpg-sign --gpg-id $GPG_FINGERPRINT --gpg-home $GPG_HOME --kms-region $KMS_REGION --aes-key-export $AES_KEY_EXPORT_PATH --gpg-key-export $GPG_KEY_EXPORT_PATH
+    lime-compiler -c /opt/limecompiler/conf/config.yml -m /opt/limecompiler/build/ -a /opt/limecompiler/archive/ --gpg-sign --gpg-id $GPG_FINGERPRINT --gpg-home $GPG_HOME --kms-region $KMS_REGION --aes-key-export $AES_KEY_EXPORT_PATH --gpg-key-export $GPG_KEY_EXPORT_PATH
     exit 0
 fi
 
 if [ `echo $GPG_SIGN` == 0 ]; then
     echo "Spinning up with without GPG Signing"
-    /opt/limecompiler/bin/lime-compiler -c /opt/limecompiler/conf/config.yml -m /opt/limecompiler/build/ -a /opt/limecompiler/archive/ --gpg-no-verify
+    lime-compiler -c /opt/limecompiler/conf/config.yml -m /opt/limecompiler/build/ -a /opt/limecompiler/archive/ --gpg-no-verify
     exit 0
 fi
